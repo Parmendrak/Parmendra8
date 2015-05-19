2ef1f9818bdb2723c62f74eb7912c95e484a71b4
parmendrak <parmendrak@360logica.com>
2015-05-19 17:47:58 +0530

    p

--- MainActivity.cs
+++ MainActivity.cs
@@ -24,7 +24,7 @@ namespace dfgfgfghfg
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+			Console.WriteLine ("Hello");
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
